0be9d3dbb7f6099f88a3d84d2f283b247612e0e9
Shimshar <sam.f.spare@outlook.com>
2020-11-25 15:37:06 +0000

    updated readGameFile (format changes needed)

--- TileGame/src/FileReader.java
+++ TileGame/src/FileReader.java
@@ -3,7 +3,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner; 
 
 public class FileReader {
-	
+
 	public static void readBoardFile(String filename) {
 		try {
 			File f = new File(filename);
@@ -47,5 +47,96 @@ public class FileReader {
       System.out.println("An error occurred.");
       e.printStackTrace();
     }
+		
+	public static void readGameFile(String filename) {
+		try {
+			File f = new File(filename);
+			Scanner s = new Scanner(f);
+			/* read player data */
+			int numPlayers = s.nextInt();
+			PlayerPiece[] players = new PlayerPiece[numPlayers];
+			TileType[][] playerHands = new String[numPlayers][10]; // assumes max hand size of 10
+			
+			for (int p = 0; p < numPlayers; p++) {
+				String name = s.next();
+				int x = s.nextInt();
+				int y = s.nextInt();
+				String colour = s.next();
+				boolean backtrackApplied = s.nextBoolean();
+				players[p] = new PlayerPiece(name, x, y, colour, backtrackApplied);
+				
+				int actionTilesOwned = s.nextInt();
+				for (int t = 0; t < actionTilesOwned; t++) {
+					String tileTypeStr = s.next();
+					TileType tileTypeEnum = TileType.valueOf(tileTypeStr);
+					String actionTileType = s.next();  // TODO: probably need to remove this
+					boolean drawnThisTurn = s.hasNextBoolean(); // TODO: need to implement this
+					playerHands[p][t] = tileTypeEnum;
+				}
+			}
+			
+			int curTurn = s.nextInt();
+			/* read the silk bag */
+			int silkBagSize = s.nextInt();
+			String[] silkBag = new String[silkBagSize];  // TODO: probably need to make this not be a string
+			for (int t = 0; t < silkBagSize; t++) {
+				silkBag[t] = s.next();
+			}			
+			/* read the game's current board */
+			int boardWidth = s.nextInt();  // TODO: maybe make reading this data a function to be reused when reading previous states?
+			int boardHeight = s.nextInt();
+			Tile[][] currentBoard = new Tile[boardWidth][boardHeight];
+			for (int x = 0; x < boardWidth; x++) {
+				for (int y = 0; y < boardHeight; y++) {
+					String tileTypeStr = s.next();
+					TileType tileTypeEnum = TileType.valueOf(tileTypeStr);
+					int rotation = s.nextInt();
+					boolean isFixed = s.hasNextBoolean();
+					String owner = s.next();
+					boolean onFire = s.hasNextBoolean();
+					boolean frozen = s.hasNextBoolean();
+					int timeRemaining = s.nextInt();
+					if (tileTypeEnum = TileType.Ice) {
+						currentBoard[x][y] = new Ice(owner, numPlayers);
+						// TODO: no function exists yet for the time remaining to be set on initialisation
+					} else if (tileTypeEnum = TileType.Fire) {
+						currentBoard[x][y] = new Fire(owner, numPlayers);
+						// TODO: see above
+					} else {
+						currentBoard[x][y] = new Placeable(tileType, isGoal, isFixed, orientation);
+					}
+				}
+			}
+			
+			int previousBoardStates = s.nextInt();
+			Tile[][][] previousBoards = new Tile[previousBoardStates][boardWidth][boardHeight];
+			for (int b = 0; b < previousBoardStates; b++) {
+				for (int x = 0; x < boardWidth; x++) {
+					for (int y = 0; y < boardHeight; y++) {
+						String tileTypeStr = s.next();
+						TileType tileTypeEnum = TileType.valueOf(tileTypeStr);
+						int rotation = s.nextInt();
+						boolean isFixed = s.hasNextBoolean();
+						String owner = s.next();
+						boolean onFire = s.hasNextBoolean();
+						boolean frozen = s.hasNextBoolean();
+						int timeRemaining = s.nextInt();
+						if (tileTypeEnum = TileType.Ice) {
+							previousBoards[b][x][y] = new Ice(owner, numPlayers);
+							// TODO: no function exists yet for the time remaining to be set on initialisation
+						} else if (tileTypeEnum = TileType.Fire) {
+							previousBoards[b][x][y] = new Fire(owner, numPlayers);
+							// TODO: see above
+						} else {
+							previousBoards[b][x][y] = new Placeable(tileType, isGoal, isFixed, orientation);
+						}
+					}
+				}
+			}
+		} catch (FileNotFoundException e){
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		}
+	}
   }
 }
