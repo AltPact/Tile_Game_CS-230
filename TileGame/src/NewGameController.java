d5b283f8ef896fe3182408053f97714818ba439a
Sunny Tong <tongsunny1999@gmail.com>
2020-11-29 17:21:15 +0000

    Remove non necessary buttons

--- TileGame/src/NewGameController.java
+++ TileGame/src/NewGameController.java
@@ -47,14 +47,6 @@ public class NewGameController extends GameWindow implements Initializable {
 	@FXML
     public Button AddButton;
 	@FXML
-    public Button Button1;
-	@FXML
-    public Button Button2;
-	@FXML
-    public Button Button3;
-	@FXML
-    public Button Button4;
-	@FXML
     public Button BackButton;
 	@FXML
     public Button StartButton;
@@ -67,7 +59,7 @@ public class NewGameController extends GameWindow implements Initializable {
 	
 	private int playerNumber=0;
 	
-	private Button[] playerNoButton = new Button[4];
+
 	
 	private ArrayList<PlayerData> playerQuene = new ArrayList<PlayerData>();
 	private ArrayList<HBox> playerRows = new ArrayList<HBox>();
@@ -76,10 +68,7 @@ public class NewGameController extends GameWindow implements Initializable {
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		//there was a stupid error without this and I didn't have time to solve it
-		playerNoButton[0]=Button1;
-		playerNoButton[1]=Button2;
-		playerNoButton[2]=Button3;
-		playerNoButton[3]=Button4;
+
 	}
 	
 	private void addPlayerInQuene(PlayerData playerToAdd) {
@@ -168,17 +157,6 @@ public class NewGameController extends GameWindow implements Initializable {
 		
 	}
 	
-	public void changeButtonColor(int number) {
-		playerNumber=number;
-		for(int i=1;i<5;i++) {
-			if(i==number) {
-				System.out.println(playerNumber);
-				playerNoButton[i-1].setStyle("-fx-background-color: WHITE; -fx-border-color: BLACK; -fx-border-width: 2.5px; -fx-text-fill: black");
-			}else {
-				playerNoButton[i-1].setStyle("-fx-background-color: rgba(128, 128, 128, 0.5); -fx-border-color: BLACK; -fx-background-insets: 0;");
-			}
-		}
-	}
 	
 	/**
 	 * This method is called when the back button is click
@@ -281,41 +259,6 @@ public class NewGameController extends GameWindow implements Initializable {
 		
 	}
 	
-	/**
-	 * This method is called when the button 1 is clicked
-	 * it will change the color of the button
-	 */
-	@FXML
-	public void buttonOnAction1(ActionEvent event) throws IOException {
-		changeButtonColor(1);
-	}
-	
-	/**
-	 * This method is called when the button 2 is clicked
-	 * it will change the color of the button
-	 */
-	@FXML
-	public void buttonOnAction2(ActionEvent event) throws IOException {
-		changeButtonColor(2);
-	}
-	
-	/**
-	 * This method is called when the button 3 is clicked
-	 * it will change the color of the button
-	 */
-	@FXML
-	public void buttonOnAction3(ActionEvent event) throws IOException {
-		changeButtonColor(3);
-	}
-	
-	/**
-	 * This method is called when the button 4 is clicked
-	 * it will change the color of the button
-	 */
-	@FXML
-	public void buttonOnAction4(ActionEvent event) throws IOException {
-		changeButtonColor(4);
- 	}
 	
 	
 
