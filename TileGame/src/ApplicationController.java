fc11e2043db9ac384baf6fa4af0fe3a097a924b1
mfirkins <mrjfirkins@gmail.com>
2020-12-04 18:11:15 +0000

    MsgBox added to closing game

--- TileGame/src/ApplicationController.java
+++ TileGame/src/ApplicationController.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import javafx.application.Application;
@@ -13,62 +14,69 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
+
 /**
- * File Name: ApplicationController.java Created: 07/11/2020 Modified: 19/11/2020
+ * File Name: ApplicationController.java Created: 07/11/2020 Modified:
+ * 19/11/2020
  * 
  * @author Wan Fai Tong (1909787) and Adem Arik (850904) Version: 1.0
  */
-public class ApplicationController extends Application{
+public class ApplicationController extends Application {
 	private static Stage stage;
-	
 
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
+
 	/**
 	 * This method starts the application window
+	 * 
 	 * @param primaryStage
 	 */
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		//Scene run
+		// Scene run
 		GameWindow gameWin = new GameWindow();
 		StackPane home = gameWin.initStackpane();
 		BorderPane homePane = FXMLLoader.load(getClass().getResource("/fxml/HomePagePane.fxml"));
 		home.getChildren().add(homePane);
 		Parent root = home;
 		Scene scene = new Scene(root, 600, 400);
-		gameWin.init(root, scene,home);
+		gameWin.init(root, scene, home);
 		primaryStage.setScene(scene);
-        stage=primaryStage;
-        primaryStage.setOnCloseRequest(e->{
-        	closeWindow(gameWin);
-        });
+		stage = primaryStage;
+		primaryStage.setOnCloseRequest(e -> {
+			closeWindow(gameWin);
+		});
 		primaryStage.show();
-		
+
 	}
+
 	/**
 	 * This method will save game data
 	 */
 	public static void closeWindow(GameWindow gameWin) {
-		if(gameWin.getCurrentGame()!= null) {
+		if (gameWin.getCurrentGame() != null) {
 			TextInputDialog newFileNameDialog = new TextInputDialog();
 			newFileNameDialog.setTitle("Save Game Data File Name");
 			newFileNameDialog.setHeaderText("Please enter a file name to save the game data");
 			newFileNameDialog.setContentText("Game Name: ");
-			Optional<String> msgBoxRespDialog = newFileNameDialog.showAndWait();
-			String newFileName = msgBoxRespDialog.get();
-			System.out.println(newFileName);
-			
+			String newFileName;
+			try {
+				Optional<String> msgBoxRespDialog = newFileNameDialog.showAndWait();
+				newFileName = msgBoxRespDialog.get();
+				System.out.println(newFileName);
+			} catch (NoSuchElementException e) {
+				System.out.println("User has selected cancel");
+
+			}
+
 		}
-		
+
 		else {
 			
 		}
-		
-		System.out.println("Save game data");
-	    stage.close();
+		stage.close();
 	}
 
 }
