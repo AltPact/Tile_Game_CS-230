9ba717e411dfa31af9a704646a942e06536a01db
mfirkins <mrjfirkins@gmail.com>
2020-12-07 08:22:39 +0000

    Formatting

--- TileGame/src/MessageOfTheDay.java
+++ TileGame/src/MessageOfTheDay.java
@@ -4,17 +4,17 @@ import java.io.InputStreamReader;
 import java.net.URL;
 
 /**
- * This class is designed to allow the message of the day to be be obtained from the 
- * website, as is prescribed in the design document. 
- * It should be instanced and then the get result method can be called from the class.
- * @author Joshua Sinderberry (851800) 
+ * This class is designed to allow the message of the day to be be obtained from
+ * the website, as is prescribed in the design document. It should be instanced
+ * and then the get result method can be called from the class.
+ * 
+ * @author Joshua Sinderberry (851800)
  * @version 1.1(Morgan Firkins: Added formatting)
  */
 public class MessageOfTheDay {
-	
+
 	/**
-	 * @private 
-	 * This method gets the puzzle in it's raw state. 
+	 * @private This method gets the puzzle in it's raw state.
 	 * @return The puzzle from the web.
 	 * @throws IOException
 	 */
@@ -25,14 +25,13 @@ public class MessageOfTheDay {
 		String inputLine = in.readLine();
 		in.close();
 		return inputLine;
-		
+
 	}
-	
+
 	/**
-	 * @private 
-	 * This method gets the final message from the web. 
+	 * @private This method gets the final message from the web.
 	 * @param param the solution to the puzzle
-	 * @return The final message from the web. 
+	 * @return The final message from the web.
 	 * @throws IOException
 	 */
 	private static String getMessage(String param) throws IOException {
@@ -44,49 +43,49 @@ public class MessageOfTheDay {
 		outputline = "Quote of the Day\n" + outputline;
 		return outputline;
 	}
-	
+
 	/**
 	 * This method gets the message of the day at at any particular time.
-	 * @return The message. 
+	 * 
+	 * @return The message.
 	 * @throws IOException
 	 */
 	public static String getResult() throws IOException {
 		String paramater = solvePuzzle(getPuzzle());
 		return getMessage(paramater);
 	}
-	
+
 	/**
-	 * @private
-	 * This method solves the puzzle as is prescribed in API.
+	 * @private This method solves the puzzle as is prescribed in API.
 	 * @param input
 	 * @return
 	 */
 	private static String solvePuzzle(String input) {
 		String output = "CS-230";
-		for(int i = 1; i <= input.length(); i++) {
-			char c = input.charAt((i-1));
+		for (int i = 1; i <= input.length(); i++) {
+			char c = input.charAt((i - 1));
 			int charValue = (int) c;
-			if(i % 2 == 0) {
-				for(int k = i; k > 0; k--) {
-					if(charValue == 90){
+			if (i % 2 == 0) {
+				for (int k = i; k > 0; k--) {
+					if (charValue == 90) {
 						charValue = 65;
 					} else {
 						charValue++;
 					}
 				}
 			} else {
-				for(int k = i; k > 0; k--) {
-					if(charValue == 65){
+				for (int k = i; k > 0; k--) {
+					if (charValue == 65) {
 						charValue = 90;
 					} else {
 						charValue--;
 					}
 				}
-				
+
 			}
 			output += (char) charValue;
 		}
-		
-		return output + output.length(); 
+
+		return output + output.length();
 	}
 }
